3b006fcf046d51a692bc2345329e1dc1f6e1053b
johnram528 <johnram528@gmail.com>
2017-08-11 14:12:25 -0400

    move App code into HomePage container

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,1 +1,21 @@
 import React from 'react'
+import NavBar from '../NavBar.js'
+import HomeSearchForm from '../components/HomeSearchForm'
+
+
+class HomePage extends Component {
+  render() {
+    return (
+        <div className='main-content-wrap'>
+          <NavBar/>
+          <div className='top-content-container'>
+            <HomeSearchForm/>
+          </div>
+          <div className='bottom-content-container'>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default HomePage;
