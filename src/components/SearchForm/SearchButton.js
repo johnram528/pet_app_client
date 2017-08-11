c49315a0efcf6aa664d991386a8aea2f11984207
johnram528 <johnram528@gmail.com>
2017-08-11 15:22:01 -0400

    buttonize search button

--- src/components/SearchForm/SearchButton.js
+++ src/components/SearchForm/SearchButton.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
 
 export default (props) => {
   return (
-                      <div className='search-form-submit col-xs-4 col-lg-4'>
-                       <i className="fa fa-paw fa-2x  " aria-hidden="true"></i>
+                      <div className='search-form-submit col-xs-4 col-lg-4' data-tip='Buscar Cuidadores'>
+                          <i className="fa fa-paw fa-2x  " aria-hidden="true"></i>
+                        <ReactTooltip place="bottom" type="light" effect="solid"/>
                       </div>)
 }
