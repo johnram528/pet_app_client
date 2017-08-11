82d3f884d68cc07e35557b7daa0242efaf62f2c2
johnram528 <johnram528@gmail.com>
2017-08-11 15:07:19 -0400

    update nav

--- src/NavBar.js
+++ src/NavBar.js
@@ -17,19 +17,12 @@ export default (props) => {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
+            <NavItem eventKey={1} href="#">Buscar Cuidadores</NavItem>
+            <NavItem eventKey={2} href="#">Hazte Cuidador</NavItem>
           </Nav>
           <Nav pullRight>
-            <NavItem eventKey={1} href="#">Link Right</NavItem>
-            <NavItem eventKey={2} href="#">Link Right</NavItem>
+            <NavItem eventKey={1} href="#">Ingresar</NavItem>
+            <NavItem eventKey={2} href="#">Registrate</NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -37,3 +30,12 @@ export default (props) => {
   </site-header>
   )
 }
+
+
+            // <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            //   <MenuItem eventKey={3.1}>Action</MenuItem>
+            //   <MenuItem eventKey={3.2}>Another action</MenuItem>
+            //   <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            //   <MenuItem divider />
+            //   <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            // </NavDropdown>
