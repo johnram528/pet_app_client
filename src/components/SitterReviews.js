84c24929311b15c36988c23ee211cfcd820499bc
johnram528 <johnram528@gmail.com>
2017-09-04 13:33:18 -0400

    review responsiveness update

--- src/components/SitterReviews.js
+++ src/components/SitterReviews.js
@@ -34,7 +34,7 @@ export default class SitterReviews extends Component {
         <div className='reviews-wrap col-xs-12'>
           <div className='reviews-data row'>
            <div className='reviews-rating-amt col-xs-4 col-lg-2'>{this.props.review_info.total + ' Reseñas' } </div>
-           <div className='reviews-rating-avg col-xs-4 col-lg-4'>
+           <div className='reviews-rating-avg col-xs-5 col-lg-4'>
             <ReactStars
               count={5}
               value={this.props.review_info.average}
