2bcff38b1d0dcc49f25e61e92d6373d4010e92d3
johnram528 <johnram528@gmail.com>
2017-09-04 17:46:33 -0400

    add sitterPreferences

--- src/components/SitterPreferences.js
+++ src/components/SitterPreferences.js
@@ -7,8 +7,39 @@ export default class SitterPreferences extends Component {
 
   }
   render(){
+    const petXsClass = this.props.preferences.sizes.includes(0) ? 'pet-size-xs col-xs-3' : 'pet-size-xs reject col-xs-3';
+    const petSmClass = this.props.preferences.sizes.includes(1) ? 'pet-size-sm col-xs-3' : 'pet-size-sm reject col-xs-3';
+    const petMdClass = this.props.preferences.sizes.includes(2) ? 'pet-size-md col-xs-3' : 'pet-size-md reject col-xs-3';
+    const petLgClass = this.props.preferences.sizes.includes(3) ? 'pet-size-lg col-xs-3' : 'pet-size-lg reject col-xs-3';
     return(
-        <div>SitterPreferences</div>
+        <div className='sitter-pref col-xs-12 col-md-4'>
+          <div className='sitter-pref-headline'>Preferencias</div>
+          <div className='sitter-pref-body row'>
+            <div className='pref-pet-sizes'>
+              <div className={petXsClass}>
+                <i className="fa fa-paw" aria-hidden="true"></i>
+                <div>0-15</div>
+                <div>Libras</div>
+              </div>
+              <div className={petSmClass}>
+                <i className="fa fa-paw" aria-hidden="true"></i>
+                <div>16-40</div>
+                <div>Libras</div>
+              </div>
+              <div className={petMdClass}>
+                <i className="fa fa-paw" aria-hidden="true"></i>
+                <div>41-100</div>
+                <div>Libras</div>
+              </div>
+              <div className={petLgClass}>
+                <i className="fa fa-paw" aria-hidden="true"></i>
+                <div>101+</div>
+                <div>Libras</div>
+              </div>
+            </div>
+          </div>
+          <div className='pref-pet-qty'>{this.props.name + ' solo puede cuidar ' + this.props.preferences.quantity + ' mascotas por noche.'}</div>
+        </div>
       )
   }
 }
--- src/containers/SitterProfile.js
+++ src/containers/SitterProfile.js
@@ -38,7 +38,8 @@ export default class SitterProfile extends Component {
       gallery: ['https://images.unsplash.com/photo-1425678013935-cafcfb4272c7?dpr=1&auto=format&fit=crop&w=1500&h=994&q=80&cs=tinysrgb&crop=', 'https://images.unsplash.com/photo-1500165974561-f16bc21927a3?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=', 'https://images.unsplash.com/photo-1456534231849-7d5fcd82d77b?dpr=1&auto=format&fit=crop&w=1500&h=846&q=80&cs=tinysrgb&crop=' ],
       bio: 'Amo a los animales, sé cómo cuidarlos y amarlos. La verdad nunca he cuidado mascotas ajenas, pero tengo 5 perros y todos ellos bien cuidados. Puedo bañarlos, sacarlos a pasear, cortarles el cabello, que tengan su siesta, etc. Estarán bien alimentamos y muy hidratados. Los puedo llevar a la veterinaria más cercana, o a la de su preferencia. Los cuídare como si fueran míos, porque en realidad sé cuánto son importantes para cada persona. Cuando sacó a pasear a mi mascota, hay muchos perros ladrando, pero no me asusta, sé cómo controlar a un perro sin asustarlo o maltratandolo. Mi novio y yo sabremos cómo cuidar a su mascota, haremos todo lo que sea necesario para que se sienta en casa. Si usted quiere, puedo bañarlos cada 15 días o cada mes, como prefiera. Los puedo alimentar con croquetas, comida enlatada, depende la edad de su mascota. También puedo entrenar a su cachorro. Puedo cuidar también en casas ajenas. Mascotas = vida feliz.',
       reviews: [{reviewer: 'test 1', date: '9-3-2017', body: 'Excelente persona con mucha calidad humana y gran cariño por las mascotas que le dejamos a su cuidado, siempre en contacto conmigo de como estaba Cushufleta, fotos y videos de mi perrhija y yo siempre feliz de no verla descuidada. Encantada con el servicio y profesionalismo, definitivamente la mejor opcion. Muy agradecida con Kariam.', rating: '5', profile_pic:'https://roneindyhiphop.files.wordpress.com/2017/02/148777547253231-e1487775574321.jpeg?quality=100&strip=all&w=1024' }, {reviewer: 'test 2', date: '9-3-2017', body: 'Excelente persona con mucha calidad humana y gran cariño por las mascotas que le dejamos a su cuidado, siempre en contacto conmigo de como estaba Cushufleta, fotos y videos de mi perrhija y yo siempre feliz de no verla descuidada. Encantada con el servicio y profesionalismo, definitivamente la mejor opcion. Muy agradecida con Kariam.', rating: '4', profile_pic: 'https://roneindyhiphop.files.wordpress.com/2017/02/148777547253231-e1487775574321.jpeg?quality=100&strip=all&w=1024'}],
-      review_info: {total:2, average:4.5}
+      review_info: {total:2, average:4.5},
+      sitter_preferences: {sizes: [0,1], quantity: 2}
     }
 
     const reviews = ( <div className='sitter-reviews col-xs-12 col-md-8'>
@@ -87,7 +88,10 @@ export default class SitterProfile extends Component {
                 <div className='bio-title'>Sobre {sitter.first_name}</div>
                 <div className='bio-body'>{sitter.bio}</div>
               </div>
-              <div className='sitter-pref col-xs-12 col-md-4'><SitterPreferences/></div>
+              <SitterPreferences
+                name={sitter.first_name}
+                preferences={sitter.sitter_preferences}
+                />
             </div>
             <div className='sitter-feedback row'>
               <div className='sitter-reviews col-xs-12 col-md-8'>
