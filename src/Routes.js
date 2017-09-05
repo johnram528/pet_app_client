825fa04d49a4a1817948f25a989f0b3601213967
johnram528 <johnram528@gmail.com>
2017-09-04 21:40:59 -0400

    create login container

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom'
 import HomePage from './containers/HomePage'
 import SearchPage from './containers/SearchPage'
+import Login from './containers/Login'
 import SitterProfile from './containers/SitterProfile'
 import createBrowserHistory from 'history/createBrowserHistory'
 
@@ -18,6 +19,7 @@ export default () => (
     <Route exact path="/" component={HomePage}/>
     <Route path="/search" component={SearchPage}/>
     <Route path="/sitters/:sitterId" component={SitterProfile}/>
+    <Route path="/login" component={Login}/>
   </div>
 </Router>
   ) 
